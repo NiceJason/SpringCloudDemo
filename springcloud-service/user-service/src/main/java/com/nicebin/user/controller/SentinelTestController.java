0f353a8fe485c8d075bb7ed1b4d437c7fa8e04dc
1437234970@qq.com <1437234970@qq.com>
2021-03-22 19:33:32 +0800

    更新

--- springcloud-service/user-service/src/main/java/com/nicebin/user/controller/SentinelTestController.java
+++ springcloud-service/user-service/src/main/java/com/nicebin/user/controller/SentinelTestController.java
@@ -29,12 +29,12 @@ public class SentinelTestController {
     @RequestMapping("/getResource2")
     public String getResource2(@RequestParam("name") String name,@RequestParam("count") int count){
         TestResource testResource = new TestResource("宝贵的资源",10);
-        return sentinelTestService.getResource1(name,count,testResource);
+        return sentinelTestService.getResource2(name,count,testResource);
     }
 
     @RequestMapping("/getResource3")
     public String getResource3(@RequestParam("name") String name,@RequestParam("count") int count){
         TestResource testResource = new TestResource("宝贵的资源",33);
-        return sentinelTestService.getResource1(name,count,testResource);
+        return sentinelTestService.getResource3(name,count,testResource);
     }
 }
